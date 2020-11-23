f989bdae485e99d1692471a11787d1a6c8abffbc
yangweitu <yangweitu@live.com>
2020-11-23 09:22:23 +0800

    添加设置文件过期时间参数

--- wcs-android-sdk/src/main/java/com/chinanetcenter/wcs/android/Config.java
+++ wcs-android-sdk/src/main/java/com/chinanetcenter/wcs/android/Config.java
@@ -7,7 +7,7 @@ public class Config {
     public static final String MGR_URL = "your manage domain";
 
 
-    public static String VERSION = "1.6.4";
+    public static String VERSION = "1.6.5";
     public static boolean DEBUGGING = false;
 
     //不设置默认url，必须由用户填充
